ab4e967d822f8ec00c0aafa53b786fb58482fa43
Jheson09 <132313915+Jheson09@users.noreply.github.com>
2023-05-02 02:51:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-vhg7yr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-vhg7yr)
---
+++ index.ts
@@ -0,0 +1,13 @@
+import './style.css';
+
+import { of, map } from 'rxjs';
+
+//of('World')
+  //.pipe(map((name) => `Hello, ${name}!`))
+  //.subscribe(console.log);
+
+// Open the console in the bottom right to see results.
+
+async function testObservable(){
+  const users=await fetch()
+}
